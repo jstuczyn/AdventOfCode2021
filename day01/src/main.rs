6e02fb6e8df736f9f388255fac29857d287f9635
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2021-12-02 19:53:32 +0000

    Ignore main function from the coverage

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -30,6 +30,7 @@ fn part2(input: &[usize]) -> usize {
         .count()
 }
 
+#[cfg(not(tarpaulin))]
 fn main() {
     let input = input_read::read_line_input("input").expect("failed to read input file");
     let start = Instant::now();
