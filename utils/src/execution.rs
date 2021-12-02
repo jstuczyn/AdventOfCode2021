9b171ed07da094d0c71a6732a62e07157a48d6fa
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2021-12-02 22:06:13 +0000

    Moved execute to execution module

--- utils/src/execution.rs
+++ utils/src/execution.rs
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::fmt::Display;
+use std::io;
+use std::path::Path;
 use std::time::{Duration, Instant};
 
 pub fn execute_slice_with_timing<F, T, U>(func: F, args: &[T]) -> (U, Duration)
@@ -22,4 +25,29 @@ where
     let res = func(args);
     let time_taken = Instant::now() - start;
     (res, time_taken)
+}
+
+// We'll see how it evolves with variety of inputs we get
+pub fn execute<P, T, F, G, H, U, S>(input_file: P, input_parser: F, part1_fn: G, part2_fn: H)
+where
+    P: AsRef<Path>,
+    F: Fn(P) -> io::Result<Vec<T>>,
+    G: Fn(&[T]) -> U,
+    H: Fn(&[T]) -> S,
+    U: Display,
+    S: Display,
+{
+    let input = input_parser(input_file).expect("failed to read input file");
+    let (part1_result, part1_time_taken) = execute_slice_with_timing(part1_fn, &input);
+    let (part2_result, part2_time_taken) = execute_slice_with_timing(part2_fn, &input);
+
+    println!(
+        "Part 1 result is {}\nIt took {:?} to compute",
+        part1_result, part1_time_taken
+    );
+
+    println!(
+        "Part 2 result is {}\nIt took {:?} to compute",
+        part2_result, part2_time_taken
+    );
 }
