16fde57f49223f372722398087711ef514b3919d
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2021-12-12 12:03:38 +0000

    Even more inefficient part2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -65,7 +65,11 @@ impl Node {
         self.name == "end"
     }
 
-    fn count_paths(&self, graph: &Graph, mut visited: HashSet<Node>) -> usize {
+    fn is_start(&self) -> bool {
+        self.name == "start"
+    }
+
+    fn count_paths(&self, graph: &Graph, mut visited: HashSet<Node>, double_visit: bool) -> usize {
         if self.is_end() {
             return 1;
         }
@@ -74,7 +78,9 @@ impl Node {
         let mut paths = 0;
         for node in graph.edges.get(self).unwrap() {
             if node.is_big || !visited.contains(node) {
-                paths += node.count_paths(graph, visited.clone())
+                paths += node.count_paths(graph, visited.clone(), double_visit)
+            } else if double_visit && !node.is_end() && !node.is_start() {
+                paths += node.count_paths(graph, visited.clone(), false)
             }
         }
         paths
@@ -104,11 +110,16 @@ fn part1(input: &[Edge]) -> usize {
         name: "start".to_owned(),
         is_big: false,
     };
-    start.count_paths(&graph, HashSet::new())
+    start.count_paths(&graph, HashSet::new(), false)
 }
 
 fn part2(input: &[Edge]) -> usize {
-    0
+    let graph = Graph::construct(input);
+    let start = Node {
+        name: "start".to_owned(),
+        is_big: false,
+    };
+    start.count_paths(&graph, HashSet::new(), true)
 }
 
 #[cfg(not(tarpaulin))]
@@ -183,5 +194,70 @@ mod tests {
         let expected = 226;
 
         assert_eq!(expected, part1(&input))
+    }
+
+    #[test]
+    fn part2_sample_input1() {
+        let input = vec![
+            "start-A".parse().unwrap(),
+            "start-b".parse().unwrap(),
+            "A-c".parse().unwrap(),
+            "A-b".parse().unwrap(),
+            "b-d".parse().unwrap(),
+            "A-end".parse().unwrap(),
+            "b-end".parse().unwrap(),
+        ];
+
+        let expected = 36;
+
+        assert_eq!(expected, part2(&input))
+    }
+
+    #[test]
+    fn part2_sample_input2() {
+        let input = vec![
+            "dc-end".parse().unwrap(),
+            "HN-start".parse().unwrap(),
+            "start-kj".parse().unwrap(),
+            "dc-start".parse().unwrap(),
+            "dc-HN".parse().unwrap(),
+            "LN-dc".parse().unwrap(),
+            "HN-end".parse().unwrap(),
+            "kj-sa".parse().unwrap(),
+            "kj-HN".parse().unwrap(),
+            "kj-dc".parse().unwrap(),
+        ];
+
+        let expected = 103;
+
+        assert_eq!(expected, part2(&input))
+    }
+
+    #[test]
+    fn part2_sample_input3() {
+        let input = vec![
+            "fs-end".parse().unwrap(),
+            "he-DX".parse().unwrap(),
+            "fs-he".parse().unwrap(),
+            "start-DX".parse().unwrap(),
+            "pj-DX".parse().unwrap(),
+            "end-zg".parse().unwrap(),
+            "zg-sl".parse().unwrap(),
+            "zg-pj".parse().unwrap(),
+            "pj-he".parse().unwrap(),
+            "RW-he".parse().unwrap(),
+            "fs-DX".parse().unwrap(),
+            "pj-RW".parse().unwrap(),
+            "zg-RW".parse().unwrap(),
+            "start-pj".parse().unwrap(),
+            "he-WI".parse().unwrap(),
+            "zg-he".parse().unwrap(),
+            "pj-fs".parse().unwrap(),
+            "start-RW".parse().unwrap(),
+        ];
+
+        let expected = 3509;
+
+        assert_eq!(expected, part2(&input))
     }
 }
